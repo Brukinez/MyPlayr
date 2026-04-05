191db28ae75638539d8de57b796030e2ff9bf482
Brukinez <simone.fardella@gmail.com>
2026-04-05 12:15:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1323,9 +1323,9 @@ elif st.session_state.pagina == 'privacy':
     
     # Incolla il tuo testo tra le triple virgolette
     testo_privacy = """ 1. Titolare del Trattamento
-Il titolare è MyClipzo (email: privacy@myclipzo.com). Per i servizi presso i centri sportivi, MyClipzo agisce come Responsabile Esterno o Contitolare insieme alla Struttura Ospitante.
+-   Il titolare è MyClipzo (email: privacy@myclipzo.com). Per i servizi presso i centri sportivi, MyClipzo agisce come Responsabile Esterno o Contitolare insieme alla Struttura Ospitante.
 
-2. Tipologia di dati raccolti
+-   2. Tipologia di dati raccolti
 Nome, cognome, email, username, dati tecnici di navigazione e contenuti video/immagini generati durante l'attività sportiva.
 3. Finalità del trattamento
 Registrazione, accesso all'area personale, generazione/download delle clip e condivisione volontaria sui social. I video sono trattati esclusivamente per scopi sportivi e ricreativi.
