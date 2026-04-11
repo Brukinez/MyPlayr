1e562ed45d97397a1c65929df5731fc1aa4cb226
Brukinez <simone.fardella@gmail.com>
2026-04-11 16:41:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -616,7 +616,7 @@ if st.session_state.pagina == 'home':
         st.markdown("<div style='text-align: center;'><span class='hero-badge'>✦ La rivoluzione dello sport amatoriale</span></div>", unsafe_allow_html=True)
         
         # Sotto il badge mettiamo il titolo grande (lo sistemeremo meglio dopo)
-        st.markdown("<h1 style='text-align: center; font-size: 80px; font-weight: 900; line-height: 1;'>GIOCA. RIVEDITI.<br><span style='color: #2ecc71;'>CONDIVIDI.</span></h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; font-size: 60px; font-weight: 900; line-height: 1;'>GIOCA. RIVEDITI.<br><span style='color: #2ecc71;'>CONDIVIDI.</span></h1>", unsafe_allow_html=True)
                 # --- DESCRIZIONE CENTRATA ---
         st.markdown("""
             <div style='text-align: center; width: 100%;'>
