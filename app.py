5fc5f6fb8ce74e2ed5e1f58db7a39d93382bf1d9
Brukinez <simone.fardella@gmail.com>
2026-04-11 09:26:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ EMERGENT_CSS = """
 
     .stat-label {
         color: #94a3b8;
-        font-size: 12px;
+        font-size: 18x;
         font-weight: 700;
         text-transform: uppercase;
         letter-spacing: 1px;
