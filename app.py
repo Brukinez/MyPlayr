8d1936331678333058bb72afba8607c664eee5fd
Brukinez <simone.fardella@gmail.com>
2026-04-11 13:44:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -258,18 +258,7 @@ EMERGENT_CSS = """
         border-radius: 8px !important;
     }
 
-    /* IL TASTO ISCRIVITI */
-    div[data-testid="stForm"] button {
-        background-color: rgb(41, 168, 71) !important;
-        color: black !important;
-        height: 50px !important;
-        width: 100% !important;
-        font-weight: 800 !important;
-        border-radius: 8px !important;
-        text-transform: uppercase !important;
-        border: none !important;
-    }
-
+ 
     /* IL TASTO ACCEDI (Sotto la scatola) */
     .login-container {
         display: flex;
