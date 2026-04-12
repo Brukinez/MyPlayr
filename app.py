42b9803727b28f0e3c2e5836086ddd55d1f849ff
Brukinez <simone.fardella@gmail.com>
2026-04-12 16:35:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -229,7 +229,9 @@ EMERGENT_CSS = """
         text-transform: uppercase;
     }
 
- 
+    .cta-final-section .highlight-green {
+        color: rgb(41, 168, 71) !important;
+    }
    
  /* Nasconde la barra grigia originale di Streamlit */
     header[data-testid="stHeader"] {
