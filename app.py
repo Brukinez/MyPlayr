9e4e6ad021faa4e6a8bdca092d1ec1a7ff8396af
Brukinez <simone.fardella@gmail.com>
2026-04-15 00:29:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1542,12 +1542,12 @@ elif st.session_state.pagina == 'profilo':
                 if f_p: 
                     st.image(f"{f_p}?t={int(time.time())}", width=150)
                 else: 
-                    st.markdown('<div style="font-size:200px; background:#3E444A; border-radius:50%; padding:20px; display:inline-block;">👤</div>', unsafe_allow_html=True)
+                    st.markdown('<div style="font-size:80px; background:#3E444A; border-radius:50%; padding:20px; display:inline-block;">👤</div>', unsafe_allow_html=True)
                 st.markdown(f"<h4>{user.get('nome', 'Atleta')}</h4>", unsafe_allow_html=True)
                 st.markdown('</div>', unsafe_allow_html=True)
             
             with cr:
-                st.markdown(f'<div style="background:#3E444A; font-size:25px; padding:20px; border-radius:10px; margin-bottom:10px;"><b>Nickname:</b> {user.get("nickname") or "N/A"}</div>', unsafe_allow_html=True)
+                st.markdown(f'<div style="background:#3E444A; font-size:25px; padding:20px; border-radius:10px; border:1px solid #28a745; margin-bottom:10px;"><b>Nickname:</b> {user.get("nickname") or "N/A"}</div>', unsafe_allow_html=True)
                 st.markdown(f'<div style="background:#3E444A; font-size:25px; padding:20px; border-radius:10px; margin-bottom:10px;"><b>Instagram:</b> {user.get("ig_tag") or "N/A"}</div>', unsafe_allow_html=True)
                 st.markdown(f'<div style="background:#3E444A; font-size:25px; padding:20px; border-radius:10px; margin-bottom:10px;"><b>Ruolo:</b> {user.get("ruolo") or "Player"}</div>', unsafe_allow_html=True)
                 st.markdown(f'<div style="background:#3E444A; font-size:25px; padding:20px; border-radius:10px; margin-bottom:10px;"><b>Email:</b> {email_sessione}</div>', unsafe_allow_html=True)
