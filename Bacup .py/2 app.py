7a76b4669bb22023cf125ce02d21a4844cdd4c8c
Brukinez <simone.fardella@gmail.com>
2026-04-17 01:11:50 +0200

    1

--- Bacup .py/2 app.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client
-
-URL_SUPABASE = "https://zxgsbcswuchrwmdcmntg.supabase.co"
-KEY_SUPABASE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4Z"
-
-# Creiamo una connessione al database Supabase che useremo sempre
-supabase = create_client(URL_SUPABASE, KEY_SUPABASE)
