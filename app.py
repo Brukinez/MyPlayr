93677dad1678c7040d5e8ffa3502a54b7d903f81
Brukinez <simone.fardella@gmail.com>
2026-04-17 02:03:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1070,8 +1070,7 @@ elif st.session_state.pagina == 'login':
             st.markdown("""
                 <div style='text-align: center; margin-bottom: 20px;'>
                     <div class='mc-box' style='display: inline-block; margin-bottom: 15px;'>MC</div>
-                    <h1 style='font-weight: 900; margin-bottom: 0; color: white;'>ACCEDI A MYCLIPZO</h1>
-                    <p style='color: #94a3b8;'>Unisciti alla community MyClipzo</p>
+                    <h1 style='font-weight: 900; margin-bottom: 0; color: white;'>ACCEDI A MYCLIPZO</h1>             
                 </div>
             """, unsafe_allow_html=True)
             
