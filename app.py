9a8626a0ba886c2eb2faa29f8bdc359905071196
Brukinez <simone.fardella@gmail.com>
2026-04-19 20:59:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1561,14 +1561,17 @@ elif st.session_state.pagina == 'home_auth':
 
     col1, col2, col3, col4 = st.columns(4)
 
-    with col1:
-        st.markdown("""
-            <div class='stat-card'>
-                <h2>🎞️</h2>
-                <h1>12</h1>
-                <p>Le tue Clip</p>
-            </div>
-        """, unsafe_allow_html=True)
+        with col1:
+            st.markdown("""
+                <div class="mcp-card">
+                    <!-- Ecco la nuova scatola dell'icona -->
+                    <div class="icon-box">
+                        <svg xmlns="http://w3.org" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#2ecc71" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m22 8-6 4 6 4V8Z"></path><rect width="14" height="12" x="2" y="6" rx="2" ry="2"></rect></svg>
+                    </div>
+                    <h3 style="color: white; margin-top: 0;">le tue clip</h3>
+                    <p style="color: #94a3b8; font-size: 18px;">Il nostro sistema registra ogni partita in alta definizione 4k senza intervento manuale.</p>
+                </div>
+            """, unsafe_allow_html=True)
 
     with col2:
         st.markdown("""
