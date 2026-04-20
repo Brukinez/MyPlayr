dad213bfd7698b3f9916ed03ebf356d9d0ce8b3d
Brukinez <simone.fardella@gmail.com>
2026-04-20 21:32:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -942,13 +942,11 @@ if st.session_state.pagina == 'home':
 
         with c1:
             st.button("INIZIA GRATIS", use_container_width=True, type="primary")
-
+            
         with c2:
             st.button("GUARDA DEMO", use_container_width=True, type="primary")
 
         st.markdown("<br>", unsafe_allow_html=True)
-
-
 
 
     # Visualizziamo i contenuti solo se l'utente non è loggato
@@ -1198,7 +1196,7 @@ elif st.session_state.pagina == 'login':
             st.markdown("""
                 <div style='text-align: center; margin-bottom: 20px;'>
                     <div class='mc-box' style='display: inline-block; margin-bottom: 15px;'>MC</div>
-                    <h1 style='font-weight: 900; margin-bottom: 0; color: white;'>CREA IL TUO ACCOUNT</h1>
+                    <h1 style='font-weight: 900; margin-bottom: 0; color: white;'>CREA IL TUO<br><span style='color: #2ecc71;'> ACCOUNT</span></h1>", unsafe_allow_html=True)
                     <p style='color: #94a3b8;'>Unisciti alla community MyClipzo</p>
                 </div>
             """, unsafe_allow_html=True)
