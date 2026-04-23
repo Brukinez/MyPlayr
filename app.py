ec29502ed2f865787b0e6cccf295d5e79d6ec325
Brukinez <simone.fardella@gmail.com>
2026-04-23 15:36:21 +0200

    1

--- app.py
+++ app.py
@@ -1103,8 +1103,8 @@ if st.session_state.pagina == 'home':
     # Reinseriamo il logo MC stile Navbar
             st.markdown("""
         <div style='display: flex; align-items: center; gap: 10px; margin-bottom: 15px;'>
-            <div class='mc-box' style='font-size: 20px; padding: 6px 10px;'>MC</div>
-            <b style='color: white; font-size: 24px;'>MyClipzo</b>
+            <div class='mc-box' style='font-size: 20px; padding: 6px 10px;'>BK</div>
+            <b style='color: white; font-size: 24px;'>BruKinez</b>
         </div>
         <p style='color: #94a3b8; font-size: 20px; line-height: 1.6;'>
             La piattaforma IoT per centri sportivi. Registra, rivedi e condividi le tue migliori azioni in campo. Lo sport amatoriale come quello vero.
